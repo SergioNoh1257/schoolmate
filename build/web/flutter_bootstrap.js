f01b4074745165264c0cf858d0c29b06817577f7
SergioNoh1257 <sergionoh1257@gmail.com>
2024-06-11 19:25:08 -0500

    Minor fix in animations

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "809386266"
+    serviceWorkerVersion: "621095845"
   }
 });
